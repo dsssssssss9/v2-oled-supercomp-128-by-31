e1bcb508dcf53f64ca94fc386df812e6672ba74c
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-05-02 21:02:56 +0100

    resolution to 128 * 63

--- main.ts
+++ main.ts
@@ -8,8 +8,8 @@ OLED12864_I2C.showString(
 )
 basic.pause(2000)
 OLED12864_I2C.clear()
-OLED12864_I2C.zoom(true)
+OLED12864_I2C.zoom(false)
 basic.forever(function () {
-    OLED12864_I2C.pixel(randint(0, 127), randint(0, 31), 1)
-    OLED12864_I2C.pixel(randint(0, 127), randint(0, 31), 0)
+    OLED12864_I2C.pixel(randint(0, 127), randint(0, 127), 1)
+    OLED12864_I2C.pixel(randint(0, 127), randint(0, 127), 0)
 })
