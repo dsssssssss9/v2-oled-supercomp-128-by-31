a23c79ae703699aa32046ca39b693e87843250fb
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-05-02 21:15:11 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let Pix_Y = 0
+let Pix_X = 0
 OLED12864_I2C.init(60)
 OLED12864_I2C.zoom(true)
 OLED12864_I2C.showString(
@@ -10,6 +12,25 @@ basic.pause(2000)
 OLED12864_I2C.clear()
 OLED12864_I2C.zoom(true)
 basic.forever(function () {
-    OLED12864_I2C.pixel(randint(0, 127), randint(0, 31), 1)
-    OLED12864_I2C.pixel(randint(0, 127), randint(0, 31), 0)
+    Pix_X = randint(0, 127)
+    Pix_Y = randint(0, 31)
+    OLED12864_I2C.rect(
+    Pix_X,
+    Pix_Y,
+    Pix_X + 1,
+    Pix_Y + 1,
+    1
+    )
+    Pix_X = randint(0, 127)
+    Pix_Y = randint(0, 31)
+    OLED12864_I2C.rect(
+    Pix_X,
+    Pix_Y,
+    Pix_X + 1,
+    Pix_Y + 1,
+    0
+    )
+})
+basic.forever(function () {
+	
 })
